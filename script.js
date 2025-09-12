7dc40804509065fc912395894b164cc33ef2967a
HARSHAREDDY7465 <68553389+HARSHAREDDY7465@users.noreply.github.com>
2025-09-12 23:55:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 async function getContinents() {
-    let result = await Xrm.WebApi.retrieveMultipleRecords("mash_continent", "?$select=mash_name,mash_continentsid");
+    let result = await Xrm.WebApi.retrieveMultipleRecords(
+        "mash_continent",
+        "?$select=mash_name,mash_continentsid"
+    );
     return result.entities;
 }
 
 async function getCountries(continentId) {
-    let result = await Xrm.WebApi.retrieveMultipleRecords("mash_country", `?$select=mash_name,mash_countrysid&$filter=_mash_continent_value eq ${continentId}`);
+    let result = await Xrm.WebApi.retrieveMultipleRecords(
+        "mash_country",
+        `?$select=mash_name,mash_countrysid&$filter=_mash_continent_value eq ${continentId}`
+    );
     return result.entities;
 }
 
 async function getStates(countryId) {
-    let result = await Xrm.WebApi.retrieveMultipleRecords("mash_state", `?$select=mash_name,mash_statesid&$filter=_mash_country_value eq ${countryId}`);
+    let result = await Xrm.WebApi.retrieveMultipleRecords(
+        "mash_state",
+        `?$select=mash_name,mash_statesid&$filter=_mash_country_value eq ${countryId}`
+    );
     return result.entities;
 }
-
-
 
 async function buildHierarchy() {
     let container = document.getElementById("treeContainer");
@@ -21,21 +28,22 @@ async function buildHierarchy() {
 
     let continents = await getContinents();
     continents.forEach(continent => {
-        let continentNode = createNode(continent.new_name, "continent");
+        // ✅ Correct field names
+        let continentNode = createNode(continent.mash_name, "continent");
         container.appendChild(continentNode);
 
         continentNode.querySelector(".expand").addEventListener("click", async () => {
             if (!continentNode.classList.contains("expanded")) {
-                let countries = await getCountries(continent.new_continentid);
+                let countries = await getCountries(continent.mash_continentsid);
                 countries.forEach(country => {
-                    let countryNode = createNode(country.new_name, "country");
+                    let countryNode = createNode(country.mash_name, "country");
                     continentNode.appendChild(countryNode);
 
                     countryNode.querySelector(".expand").addEventListener("click", async () => {
                         if (!countryNode.classList.contains("expanded")) {
-                            let states = await getStates(country.new_countryid);
+                            let states = await getStates(country.mash_countrysid);
                             states.forEach(state => {
-                                let stateNode = createNode(state.new_name, "state");
+                                let stateNode = createNode(state.mash_name, "state");
                                 countryNode.appendChild(stateNode);
                             });
                             countryNode.classList.add("expanded");
@@ -60,10 +68,6 @@ function createNode(name, type) {
 }
 
 function collapseNode(node) {
-    [...node.children].forEach(child => {
-        if (!child.classList.contains("expand")) {
-            child.remove();
-        }
-    });
+    [...node.querySelectorAll(".node")].forEach(child => child.remove());
     node.classList.remove("expanded");
 }
